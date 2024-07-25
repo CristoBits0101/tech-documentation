7913b7b88589a5dec8e656561c31d569b30d365f
Cristo RPS <cristobits0101@gmail.com>
2024-07-25 14:06:23 +0100

    Actualizando

---
+++ backend/Spring/code/udemy/ioc/springboot-di[2-autowired-constructor-setter]/src/main/java/com/andres/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.andres.springboot.di.app.springbootdi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.andres.springboot.di.app.springbootdi.models.Product;
+import com.andres.springboot.di.app.springbootdi.repositories.ProductoRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    
+    private ProductoRepository repository;
+
+    public ProductServiceImpl(ProductoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return repository.findAll().stream().map(p -> {
+            Double priceTax = p.getPrice() * 1.25d;
+            // Product newProd = new Product(p.getId(), p.getName(), priceTax.longValue());
+            Product newProd = (Product) p.clone();
+            newProd.setPrice(priceTax.longValue());
+            return newProd;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return repository.findById(id);
+    }
+
+
+
+}
