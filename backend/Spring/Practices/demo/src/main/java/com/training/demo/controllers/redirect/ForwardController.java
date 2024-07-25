8119d6f2157cf8ec281fb3d635a7935cf0ad5811
Cristo RPS <cristobits0101@gmail.com>
2024-07-25 12:30:08 +0100

    Actualizando rutas

--- backend/Spring/Practices/demo/src/main/java/com/training/demo/controllers/redirect/ForwardController.java
+++ backend/Spring/Practices/demo/src/main/java/com/training/demo/controllers/redirect/ForwardController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ForwardController {
 
-    
     @GetMapping("/forward-to-bye")
     public String forwardController() {
         return "forward:bye";
--- backend/Spring/Practices/demo/src/main/java/com/training/demo/controllers/types/HybridController.java
+++ backend/Spring/Practices/demo/src/main/java/com/training/demo/controllers/types/HybridController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HybridController {
+    
     @GetMapping("/cat")
     @ResponseBody
     public String hello() {
