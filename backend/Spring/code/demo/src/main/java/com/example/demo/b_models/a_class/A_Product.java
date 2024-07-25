cfdb60e9ac8b65be6e1603e1f536bf9a574539a2
Cristo RPS <cristobits0101@gmail.com>
2024-07-25 18:54:12 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/b_models/a_class/A_Product.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/b_models/a_class/A_Product.java
@@ -1,16 +1,54 @@
 package com.example.demo.b_models.a_class;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
+@Component
 public class A_Product {
 
+    // Atributes
     private Long id;
     private String name;
     private int price;
+
+    // Constructors
+    public A_Product() {
+    }
+
+    public A_Product(Long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Setters and getters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "A_Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
 
 }
