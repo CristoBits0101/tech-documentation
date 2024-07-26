8672dd8bceb98b6edb81477fdefd6caf03068236
Cristo RPS <cristobits0101@gmail.com>
2024-07-26 19:35:27 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/h_exceptions/hb_controller/hbb_advice/hbba_global/HBBA_HandlerExceptionController.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/h_exceptions/hb_controller/hbb_advice/hbba_global/HBBA_HandlerExceptionController.java
@@ -16,20 +16,24 @@ public class HBBA_HandlerExceptionController {
     @ExceptionHandler(ArithmeticException.class)
     public ResponseEntity<?> divisionByZeroA(Exception e) {
 
-        // return ResponseEntity
-        //     .status("500")
-        //     .body("Error 500");
-
-        // return ResponseEntity
-        //         .internalServerError()
-        //         .body("Error 500");
-
         HAA_Error error = new HAA_Error();
 
         error.setError("Error: Division by zero.");           // Personalizado
         error.setMessage(e.getMessage());                           // Pertenece a Exception e
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());  // Pertenece a HttpStatus de Spring
         error.setDate(new Date());                                  // Pertenece a java.util.Date
+
+        // return ResponseEntity
+        //     .status("500")
+        //     .body("Error 500");
+
+        // return ResponseEntity
+        //         .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        //         .body(error);
+
+        // return ResponseEntity
+        //         .internalServerError()
+        //         .body("Error 500");
 
         return ResponseEntity
             .internalServerError()
