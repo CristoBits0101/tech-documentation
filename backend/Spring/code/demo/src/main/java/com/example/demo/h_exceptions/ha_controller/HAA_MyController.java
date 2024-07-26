28bbaae9e0b7e2af84528d72c7cd23402b0b948b
Cristo RPS <cristobits0101@gmail.com>
2024-07-26 18:51:14 +0100

    u

---
+++ backend/Spring/code/demo/src/main/java/com/example/demo/h_exceptions/ha_controller/HAA_MyController.java
@@ -0,0 +1,16 @@
+package com.example.demo.h_exceptions.ha_controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HAA_MyController {
+    
+    // 
+    @GetMapping("index")
+    public int index() {
+        int value = 100 / 0;
+        return value;
+    }
+
+}
