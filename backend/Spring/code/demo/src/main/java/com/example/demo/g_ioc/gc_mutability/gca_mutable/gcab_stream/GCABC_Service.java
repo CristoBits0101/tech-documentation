ef6931d71ad37e471976dea0919837c6a06ed7bf
Cristo RPS <cristobits0101@gmail.com>
2024-07-26 11:28:07 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/gc_mutability/gca_mutable/gcab_stream/GCABC_Service.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/gc_mutability/gca_mutable/gcab_stream/GCABC_Service.java
@@ -1,10 +1,10 @@
 package com.example.demo.g_ioc.gc_mutability.gca_mutable.gcab_stream;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/mutable-stream")
-@RestController
+@Service
 public class GCABC_Service {
 
 }
--- backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/gc_mutability/gca_mutable/gcab_stream/GCABD_Repository.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/gc_mutability/gca_mutable/gcab_stream/GCABD_Repository.java
@@ -1,10 +1,10 @@
 package com.example.demo.g_ioc.gc_mutability.gca_mutable.gcab_stream;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/mutable-stream")
-@RestController
+@Repository
 public class GCABD_Repository {
 
 }
