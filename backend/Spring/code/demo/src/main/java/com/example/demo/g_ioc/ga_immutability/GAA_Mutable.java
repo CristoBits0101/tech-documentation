69e973bfea5092014667a796408f5e3d9881bed2
Cristo RPS <cristobits0101@gmail.com>
2024-07-26 01:03:48 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/ga_immutability/GAA_Mutable.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/ga_immutability/GAA_Mutable.java
@@ -10,16 +10,68 @@ import java.util.stream.Collectors;
 @RestController
 public class GAA_Mutable {
 
-    // Singleton Attribute mutable
-    private List<Integer> price = Arrays.asList(1, 2, 3);
+    // Mutable Singleton Attributes
+    private Long id;
+    private String name;
+    private int price;
+    private List<GAA_Mutable> data;
+    
+    public GAA_Mutable() {
+        this.data = Arrays.asList(
+                new GAA_Mutable(1L, "Memoria corsair 32", 300),
+                new GAA_Mutable(2L, "Cpu Intel Core i9", 850),
+                new GAA_Mutable(3L, "Teclado Razer Mini 60%", 180),
+                new GAA_Mutable(4L, "Motherboard Gigabyte", 490));
+    }
 
-    @GetMapping("mutable")
-    public List<Integer> getPrice() {
-        price
-            .stream()                       // Convert to stream
-            .map(p -> p *= 2)               // Return the result
-            .collect(Collectors.toList());  // Convert to list
+    public GAA_Mutable(Long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    GAA_Mutable gaaMutable = new GAA_Mutable();
+
+    @GetMapping("/mutable")
+    public List<GAA_Mutable> mutable() {
+        gaaMutable
+            .data
+            .stream()
+            .map(p -> p.getPrice() * 2)
+            .collect(Collectors.toList());
+        return gaaMutable.data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
         return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public List<GAA_Mutable> getData() {
+        return data;
+    }
+
+    public void setData(List<GAA_Mutable> data) {
+        this.data = data;
     }
 
 }
--- backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/ga_immutability/GAB_ImmutabilityStream.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/ga_immutability/GAB_ImmutabilityStream.java
@@ -1,21 +1,23 @@
 package com.example.demo.g_ioc.ga_immutability;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
+@RestController
 public class GAB_ImmutabilityStream {
 
-    // Singleton Attribute mutable
+    // Mutable Singleton Attributes
     private List<Integer> price = Arrays.asList(1, 2, 3);
 
-    @GetMapping("mutable")
+    @GetMapping("/immutability-stream")
     public List<Integer> getPrice() {
         List<Integer> newPrice = price      // Guarda el retorno del map
             .stream()                       // Copia la lista con el tipo stream
-            .map(p -> p *= 2)               // Retorna el resultado
+            .map(p -> p * 2)                // Retorna el resultado
             .collect(Collectors.toList());  // Convierte el stream en lista
         return newPrice;
     }
