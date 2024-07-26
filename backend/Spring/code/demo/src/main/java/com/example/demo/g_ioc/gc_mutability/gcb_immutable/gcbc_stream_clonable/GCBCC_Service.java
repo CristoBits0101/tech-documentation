365da69fc2ba12e116dc20c7faf2c806a44b1199
Cristo RPS <cristobits0101@gmail.com>
2024-07-26 15:32:54 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/gc_mutability/gcb_immutable/gcbc_stream_clonable/GCBCC_Service.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/g_ioc/gc_mutability/gcb_immutable/gcbc_stream_clonable/GCBCC_Service.java
@@ -3,7 +3,6 @@ package com.example.demo.g_ioc.gc_mutability.gcb_immutable.gcbc_stream_clonable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,17 +12,24 @@ public class GCBCC_Service {
     @Autowired
     GCBCD_Repository repository;
 
+    /**
+     * Importante:
+     * Con stream se trabaja sobre una vista que no modifica la original
+     * Pero el objeto que almacena en memoria es el mismo que el de la original
+     * 
+     * @return
+     */
     public List<GCBCA_Model> findAll() {
-        repository
-            .findAll()                                          // 1. Obtenemos la Lista Original
-            .stream()                                           // 2. Trabaja sobre una vista que no modifica la original
-            .map(p -> {                                         // 3. Transforma los elementos en el flujo.
-                GCBCA_Model newModel = (GCBCA_Model) p.clone(); // 4. Clonamos el objeto original
-                double newPrice = p.getPrice() * 2;             // 5. Copiamos el precio original incrementado
-                newModel.setPrice(newPrice);                    // 6. Añadimos el nuevo precio al clon
-                return newModel;                                // 7. Retornamos el clon modificado.
+        return repository
+                .findAll() // 1. Obtenemos la Lista Original
+                .stream() // 2. Trabaja sobre una vista que no modifica la original
+                .map(p -> { // 3. Transforma los elementos en el flujo.
+                    GCBCA_Model newModel = (GCBCA_Model) p.clone(); // 4. Clonamos el objeto original
+                    double newPrice = p.getPrice() * 2; // 5. Copiamos el precio original incrementado
+                    newModel.setPrice(newPrice); // 6. Añadimos el nuevo precio al clon
+                    return newModel; // 7. Retornamos el clon modificado.
                 })
-            .collect(Collectors.toList());                      // 8. Parseamos el stream a lista.
+                .collect(Collectors.toList()); // 8. Convierte el stream a una lista para poder retornarla en la función
     }
-    
+
 }
