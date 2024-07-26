5cc1b8c0d16ff5bd8925293be175f05b83aa676f
Cristo RPS <cristobits0101@gmail.com>
2024-07-26 19:39:24 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/h_exceptions/hb_controller/hbb_advice/hbba_global/HBBA_HandlerExceptionController.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/h_exceptions/hb_controller/hbb_advice/hbba_global/HBBA_HandlerExceptionController.java
@@ -1,44 +1,8 @@
 package com.example.demo.h_exceptions.hb_controller.hbb_advice.hbba_global;
 
-import java.util.Date;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.example.demo.h_exceptions.ha_model.HAA_Error;
 
 @ControllerAdvice
 public class HBBA_HandlerExceptionController {
-
-    // 
-    @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> divisionByZeroA(Exception e) {
-
-        HAA_Error error = new HAA_Error();
-
-        error.setError("Error: Division by zero.");           // Personalizado
-        error.setMessage(e.getMessage());                           // Pertenece a Exception e
-        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());  // Pertenece a HttpStatus de Spring
-        error.setDate(new Date());                                  // Pertenece a java.util.Date
-
-        // return ResponseEntity
-        //     .status("500")
-        //     .body("Error 500");
-
-        // return ResponseEntity
-        //         .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-        //         .body(error);
-
-        // return ResponseEntity
-        //         .internalServerError()
-        //         .body("Error 500");
-
-        return ResponseEntity
-            .internalServerError()
-            .body(error);
-
-    }
 
 }
---
+++ backend/Spring/code/demo/src/main/java/com/example/demo/h_exceptions/hb_controller/hbb_advice/hbbb_modular/HBBA_DivisionByZeroStatus500.java
@@ -0,0 +1,44 @@
+package com.example.demo.h_exceptions.hb_controller.hbb_advice.hbbb_modular;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.demo.h_exceptions.ha_model.HAA_Error;
+
+@ControllerAdvice
+public class HBBA_DivisionByZeroStatus500 {
+
+    // Error en el código no controlado
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<?> divisionByZero(Exception e) {
+
+        HAA_Error error = new HAA_Error();
+
+        error.setError("Error: Division by zero.");                 // Personalizado
+        error.setMessage(e.getMessage());                           // Pertenece a Exception e
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());  // Pertenece a HttpStatus de Spring
+        error.setDate(new Date());                                  // Pertenece a java.util.Date
+
+        // return ResponseEntity
+        //     .status("500")
+        //     .body("Error 500");
+
+        // return ResponseEntity
+        //         .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        //         .body(error);
+
+        // return ResponseEntity
+        //         .internalServerError()
+        //         .body("Error 500");
+
+        return ResponseEntity
+            .internalServerError()
+            .body(error);
+
+    }
+
+}
