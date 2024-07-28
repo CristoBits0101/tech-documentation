d2a0265385e0ffb35eacdab73dc9a23e1fe5c4e6
Cristo RPS <cristobits0101@gmail.com>
2024-07-28 12:37:17 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.k_jpa.kb_entities.PersonEntity;
+import com.example.demo.k_jpa.kc_repositories.PersonRepository;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	PersonRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// Desde main accedemos a repository sin controlador ni service 
+		List<PersonEntity> persons = (List<PersonEntity>) repository.findAll();
+	
 	}
 
 }
