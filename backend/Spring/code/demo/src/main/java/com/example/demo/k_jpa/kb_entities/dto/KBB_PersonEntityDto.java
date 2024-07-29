3e48260e22b3ed3f560f4640fe8fe27a68dd27ee
Cristo RPS <cristobits0101@gmail.com>
2024-07-29 11:48:06 +0100

    u

---
+++ backend/Spring/code/demo/src/main/java/com/example/demo/k_jpa/kb_entities/dto/KBB_PersonEntityDto.java
@@ -0,0 +1,43 @@
+package com.example.demo.k_jpa.kb_entities.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persons")
+public class KBB_PersonEntityDto {
+
+    private String name;
+    private String lastname;
+
+    public KBB_PersonEntityDto(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDto [name=" + name + ", lastname=" + lastname + "]";
+    }
+
+}
