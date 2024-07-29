5affb469157b0cfec2571579e3f8db2acc4adcae
Cristo RPS <cristobits0101@gmail.com>
2024-07-29 16:23:13 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/k_jpa/kb_entities/dto/KBB_PersonEntityDto.java
+++ backend/Spring/code/demo/src/main/java/com/example/demo/k_jpa/kb_entities/dto/KBB_PersonEntityDto.java
@@ -1,10 +1,6 @@
 package com.example.demo.k_jpa.kb_entities.dto;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
