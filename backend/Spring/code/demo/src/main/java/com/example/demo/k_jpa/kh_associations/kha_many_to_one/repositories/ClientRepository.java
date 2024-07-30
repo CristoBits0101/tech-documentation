a3fa422b2dd5b7627a59ef59eaf4b96e364056d9
Cristo RPS <cristobits0101@gmail.com>
2024-07-30 06:54:58 +0100

    u

--- backend/Spring/code/demo/src/main/java/com/example/demo/k_jpa/kh_associations/kha_many_to_one/repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.k_jpa.kh_associations.kha_many_to_one.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.k_jpa.kh_associations.kha_many_to_one.entities.Client;
-
-public interface ClientRepository extends CrudRepository<Client, Long>{
-    
-}
