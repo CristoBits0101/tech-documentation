da64a28053ea3dc54f6abcd4624eee12698d8e6e
Cristo RPS <cristobits0101@gmail.com>
2024-07-30 06:09:35 +0100

    u

---
+++ backend/Spring/code/demo/src/main/java/com/example/demo/k_jpa/kh_associations/kha_many_to_one/repositories/InvoiceRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.k_jpa.kh_associations.kha_many_to_one.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.k_jpa.kh_associations.kha_many_to_one.entities.Invoice;
+
+public interface InvoiceRepository extends CrudRepository<Invoice, Long>{
+    
+}
