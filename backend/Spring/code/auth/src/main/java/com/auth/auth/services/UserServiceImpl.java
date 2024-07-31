8a04ae6e0c6156099374b5175bb6138b3f45d056
Cristo RPS <cristobits0101@gmail.com>
2024-07-31 19:22:53 +0100

    u

---
+++ backend/Spring/code/auth/src/main/java/com/auth/auth/services/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.auth.auth.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.auth.auth.entities.User;
+import com.auth.auth.repositories.UserRepository;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+}
