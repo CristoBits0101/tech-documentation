b2123e9a71415080c52078b18597482223375bb0
Cristo RPS <cristobits0101@gmail.com>
2024-08-08 17:42:38 +0100

    u

--- frontend/TypeScript/Midudev/index.ts
+++ frontend/TypeScript/Midudev/index.ts
@@ -1,8 +1,8 @@
 /**
  * Adds two numbers.
  * 
- * @param {number} a
- * @param {number} b
+ * @param   {number} a
+ * @param   {number} b
  * @returns {number} a + b
  */
 function suma(a: number, b: number): number {
