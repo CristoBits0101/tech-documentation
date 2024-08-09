d37f3e542a5a10e2282860bfc901cc5949313ab2
Cristo RPS <cristobits0101@gmail.com>
2024-08-09 17:51:28 +0100

    U

--- frontend/TypeScript/Midudev/26_HTMLCanvasElement.ts
+++ frontend/TypeScript/Midudev/26_HTMLCanvasElement.ts
@@ -1,5 +1,11 @@
 const canvas = document.createElement('canvas') as HTMLCanvasElement;
 
+/**
+ * Si lo encuentra devuelte HTMLElement
+ * Si no lo encuentra devuelve null
+ * Para saber si es canvas usar la propiedad HTMLCanvasElement
+ * HTMLCanvasElement es porque canvas no es un elemento HTML
+ */
 if (canvas !== null) {
     const ctx = canvas.getContext('2d');
 }
