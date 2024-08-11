de6a6728fc6e45ac9bb6761497b7a739319011a2
Cristo RPS <cristobits0101@gmail.com>
2024-08-11 22:26:38 +0100

    u

--- frontend/React/Practices/api-fetch/src/App.jsx
+++ frontend/React/Practices/api-fetch/src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const FACT_URL = "https://catfact.ninja/fact"
+  const IMAGE_URL_PREFIX = "https://cataas.com/cat/says/"
+  const IMAGE_URL = "https://cataas.com"
+
+  // Paso 1)
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  // Paso 2)
+  useEffect(() => {
+    fetch(FACT_URL)
+      .then((response) => response.json())
+      .then((json) => {
+        const { fact } = json
+        setFact(fact)
+        const firstWord = fact.split(" ", 3)[0]
+        fetch(`${IMAGE_URL_PREFIX}${firstWord}?size=50&color=red&json=true`)
+          .then((response) => response.json())
+          .then((response) => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Paso 3) */}
+      <main>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={`${IMAGE_URL}${imageUrl}`}
+            alt={`Imagen generada usando la primera palabra de: ${fact}`}
+          />
+        )}
+      </main>
     </>
   )
 }
