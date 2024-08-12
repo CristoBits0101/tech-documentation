8a7303ebbe75b3057d15c897848d30b11269459e
Cristo RPS <cristobits0101@gmail.com>
2024-08-12 15:25:39 +0100

    u

--- frontend/React/Practices/api-fetch/src/App.jsx
+++ frontend/React/Practices/api-fetch/src/App.jsx
@@ -13,23 +13,35 @@ function App() {
   // Paso 2)
   useEffect(() => {
     fetch(FACT_URL)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error! status: ${response.status}`)
+        response.json();
+      })
       .then((json) => {
         const { fact } = json
         setFact(fact)
-        const firstWord = fact.split(" ", 3)[0]
-        fetch(`${IMAGE_URL_PREFIX}${firstWord}?size=50&color=red&json=true`)
-          .then((response) => response.json())
-          .then((response) => {
-            const { url } = response
-            setImageUrl(url)
-          })
       })
+      .catch((error) => console.error("Error fetching fact:", error))
   }, [])
+
+  // Paso 3)
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(" ")[0]
+    fetch(`${IMAGE_URL_PREFIX}${firstWord}?size=50&color=red&json=true`)
+      .then((response) => response.json())
+      .then((response) => {
+        const { url } = response
+        console.log("Image URL:", `${IMAGE_URL}${url}`)
+        setImageUrl(`${IMAGE_URL}${url}`)
+      })
+      .catch((error) => console.error("Error fetching image:", error))
+  }, [fact])
 
   return (
     <>
-      {/* Paso 3) */}
+      {/* Paso 4) */}
       <main>
         {fact && <p>{fact}</p>}
         {imageUrl && (
