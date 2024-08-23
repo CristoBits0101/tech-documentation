842b155537c0c945288094fc8299b6559593fdcb
Cristo RPS <cristobits0101@gmail.com>
2024-08-23 18:52:32 +0100

    Cast refactoring

--- frontend/JavaScript/ArrayLists/Cast.js
+++ frontend/JavaScript/ArrayLists/Cast.js
@@ -4,7 +4,9 @@ const indices = Array.from(array.keys()); // [0, 1, 2, ..., 12]
 
 let resultado = indices                   // Duplicar las letras.
   .map((index) => {
-    return index % 2 === 0 ? array[index].repeat(2) : array[index];
+    return index % 2 === 0   ? 
+      array[index].repeat(2) : 
+      array[index];
   })
   .join("");
 
