07bc6a5aa7d9128f29c90171aa24678229177889
Cristo RPS <cristobits0101@gmail.com>
2024-08-23 18:43:34 +0100

    Refactoring

--- frontend/JavaScript/ArrayLists/Concatenate.js
+++ frontend/JavaScript/ArrayLists/Concatenate.js
@@ -1,31 +1,12 @@
-// Declaración.
 const lista1 = [1, "1", null, undefined, false];
 const lista2 = new Array(1, "1", null, undefined, false);
 const lista3 = new Array();
 
-// Unir con concat.
-lista3 = lista1.concat(lista2);
+lista3 = lista1.concat(lista2);               // Unir con concat.
+let lista4 = [...lista1, ...lista2];          // Unir con spread.
+let lista5 = [lista1, lista2];                // Unir en una matriz.
 
-// Unir listas utilizando el factor de dispersión.
-let lista4 = [...lista1, ...lista2];
-
-// Unir listas en dos posiciones diferentes en una matriz.
-let lista5 = [lista1, lista2];
-
-const array1 = [1, 234, 'nbdsfxg3'];
-
-// Concatenar un valor a posiciones.
-const array2 = array.map( nameVariable => 
-{
-    return array.concat(" l");
+const lista6 = [1, 234, "nbdsfxg3"];          // Alterar posiciones.
+const lista7 = array1.map((nameVariable) => {
+  return nameVariable.concat(" l");
 });
-
-// Agregando un valor como prefijo.
-const array3 = array.map( nameVariable => 
-{
-    return `1 - ${nameVariable}`;
-});
-
-// Agregar un valor como prefijo con index.
-const array4 = array.map((nameVariable, index) => {return `${index} - ${nameVariable}`;});
-const array5 = array.map((nameVariable, index) => `${index + 1} - ${nameVariable}`);
