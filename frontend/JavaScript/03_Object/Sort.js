875e3753315b5409020a40c1b1f855c415500dee
Cristo RPS <cristobits0101@gmail.com>
2024-08-24 10:42:44 +0100

    Updating

---
+++ frontend/JavaScript/03_Object/Sort.js
@@ -0,0 +1,10 @@
+const objectList = [
+  { nombre: Crista, edad: 31 },
+  { nombre: Criste, edad: 32 },
+  { nombre: Cristi, edad: 33 },
+];
+
+// Ordenar por nombre.
+objectList.sort = (a, b) => {
+  a.nombre - b.nombre;
+};
--- frontend/JavaScript/09_Objects/Sort.js
+++
@@ -1,9 +0,0 @@
-const objectList = 
-[
-    {nombre: Crista, edad: 31},
-    {nombre: Criste, edad: 32},
-    {nombre: Cristi, edad: 33}
-]
-
-// Ordenar por titulo.
-objectList.sort = (a, b) => {a.nombre - b.nombre}
