1d6ab64bef942cd70925c67178bbc1e0f7d40fd3
Cristo RPS <cristobits0101@gmail.com>
2024-09-05 16:34:55 +0100

    Refactoring

--- backend/Node/11_integrated-modules-fs.js
+++ backend/Node/11_integrated-modules-fs.js
@@ -6,3 +6,30 @@ const stats = fs.statSync("./data.txt");
 
 // 3) Muestra el tipo de fichero
 console.log(stats.isFile, stats.isDirectory, stats.isSymbolicLink, stats.size);
+
+// 4) Leer el directorio actual y mostrar los ficheros
+fs.readdir(".", (error, files) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  files.forEach((file) => console.log(file));
+});
+
+// 5) Comprobación asíncrona de si el fichero existe con stats
+fs.access("./data.txt", fs.constants.F_OK, (error) => {
+  if (error) {
+    console.error("File does not exist:", error);
+  } else {
+    console.log("File exists");
+  }
+});
+
+// 6) Comprobación síncrona de si el fichero existe con stats
+if (fs.existsSync("./data.txt")) {
+  console.log("File exists");
+} else {
+  console.log("File does not exist");
+}
+
+// 7) 
