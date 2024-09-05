e636049b54f0da9694edfd8c8bdbc697c917e1cc
Cristo RPS <cristobits0101@gmail.com>
2024-09-05 15:22:53 +0100

    Updating

--- backend/Node/19_integrated-modules-path.js
+++ backend/Node/19_integrated-modules-path.js
@@ -1,8 +1,21 @@
-// 1) Módulo para el sistema de ficheros
-const fs = require("node:fs");
+// 1) Importar módulo para trabajar con las rutas en node
+const path = require("node:path");
 
-// 2) Obtiene el fichero de manera sincrónica
-const stats = fs.statSync("./data.txt");
+// 2) Saber que tipos de barras usa mi sistema operativo
+console.log(path.sep);
 
-// 3) Muestra el tipo de fichero
-console.log(stats.isFile, stats.isDirectory, stats.isSymbolicLink, stats.size);
+// 3) Unir rutas
+const filePath = path.join("content", "subfolder", "test.js");
+console.log(filePath);
+
+// 4) Obtener el nombre del fichero
+const base = path.basename(filePath);
+console.log(base);
+
+// 4) Obtener el nombre del fichero sin la extención
+const filename = path.basename(filePath, ".js");
+console.log(filename);
+
+// 5) Obtener la extensión del fichero
+const ext = path.extname(filePath);
+console.log(ext);
