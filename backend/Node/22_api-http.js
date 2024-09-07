0b8266674d5cfe2ce8a0a48890fa7f4d2997a80e
Cristo RPS <cristobits0101@gmail.com>
2024-09-07 21:53:55 +0100

    Updating commets

--- backend/Node/22_api-http.js
+++ backend/Node/22_api-http.js
@@ -1,19 +1,23 @@
-// Protocolo HTTP
+// 1) Importar el módulo de node
 const http = require('node:http')
 
-// Importa la función que busca un puerto libre
+// 2) Importa la función que busca un puerto libre
 const { findAvailablePort } = require('./23._free-port.js')
 
-// Inprime la variable para el port
+// 3) Inprime la variable para el port
 console.log(process.env)
 
-// Puerto desde el que escucha las peticiones
+// 4) Puerto desde el que escucha las peticiones
 const desiredPort = process.env.PORT ?? 3000
 
-// Callback que gestiona las request and response
-const server = http.createServer((req, res) => {
+/**
+ * 5) Creación de un servidor web
+ * Gestiona las peticiones y las respuestas
+ * El método recibe un callback
+ */
+const server = http.createServer((request, response) => {
     console.log('request received')
-    res.end('Hola mundo')
+    response.end('Hola mundo')
 })
 
 // Encuentra un puerto libre
