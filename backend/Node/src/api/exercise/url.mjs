3a33720e4a1555c89d922a1b7918ba3b85a1f570
Cristo RPS <cristobits0101@gmail.com>
2024-09-08 14:13:37 +0100

    Adding url and status code

--- backend/Node/src/api/exercise/url.mjs
+++ backend/Node/src/api/exercise/url.mjs
@@ -1,11 +1,19 @@
 export const processRequest = (req, res) => {
-  if (req === '/') {
+  // Test
+  console.log('Received request.')
+
+  // Request
+  if (req.url === '/') {
     // Header
-    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Content-Type', 'text/html; charset=utf8')
     res.statusCode = 200
     // Body
-    res.end('Welcome to the home page!')
+    res.end('<h1>¡Bienvenido a mí página principal!</h1>')
+  } else {
+    // Header
+    res.setHeader('Content-Type', 'text/plain; charset=utf8')
+    res.statusCode = 404
+    // Body
+    res.end(' 404 | Page Not Found')
   }
-  console.log('Received request.')
-  res.end('Hello, World!')
 }
