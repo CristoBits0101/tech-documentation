9293cc4d6baade35353c0e52ad865283fd998075
Cristo RPS <cristobits0101@gmail.com>
2024-09-08 10:11:22 +0100

    Refactoring

--- backend/Node/concurrence/async/fs-await-parallel.js
+++ backend/Node/concurrence/async/fs-await-parallel.js
@@ -5,7 +5,7 @@ const { fs } = require('node:fs/promises')
 Promise.all([
   fs('./data.txt', 'utf8'),
   fs('./info.txt', 'utf8'),
-  fs('./text.txt', 'utf8'),
+  fs('./text.txt', 'utf8')
 ]).then(([text, secondText]) => {
   console.log(text)
   console.log(secondText)
