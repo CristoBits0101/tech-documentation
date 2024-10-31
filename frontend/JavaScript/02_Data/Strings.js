eb482386f37cfdf6ab9e6e6dbe99bff8c9accd90
Cristo RPS <cristobits0101@gmail.com>
2024-10-31 21:18:39 +0000

    Refactoring Strings

---
+++ frontend/JavaScript/02_Data/Strings.js
@@ -0,0 +1,30 @@
+// Comparison
+console.log(text1.toLowerCase() === text2.toLowerCase())
+console.log(text1.toUpperCase() === text2.toUpperCase())
+
+// Concatenate
+const saludo = 'Hola'
+const nombre = 'Cristo'
+
+console.log(saludo.concat(nombre) + '\n')
+console.log(saludo.concat(' ', nombre) + '\n')
+console.log(saludo + ' ' + nombre + '\n')
+console.log(`${saludo} ${nombre} \n`)
+
+// Count
+
+// Include
+
+// Length
+
+// Limit
+
+// Posisition
+
+// Replace
+
+// Resize
+
+// Spaces
+
+// Types
--- frontend/JavaScript/02_Data/Strings/Comparison.js
+++
@@ -1,2 +0,0 @@
-console.log(text1.toLowerCase() === text2.toLowerCase()); // Comparación en minúsculas
-console.log(text1.toUpperCase() === text2.toUpperCase()); // Comparación en mayúsculas
