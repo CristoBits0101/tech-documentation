cff149e59cadb4dcc5dcd9936ffe2af8b999d0db
Cristo RPS <cristobits0101@gmail.com>
2024-10-31 14:49:41 +0000

    Updating comments

--- frontend/JavaScript/01_Environment/NPM.js
+++ frontend/JavaScript/01_Environment/NPM.js
@@ -1,11 +1,9 @@
 /**
- * 1. node file_name
+ * 1.        npm init -> Inicializar Node.js + parámetros
+ * 2.     npm init -y -> Inicializar Node.js - parámetros
  * 
- * 2. npm init
+ * 3.  node file_name -> Ejecutar archivo JavaScript
+ * 4. npm script_name -> Ejecutar  script JavaScript
  * 
- * 3. npm init -y
- * 
- * 4. npm install --global http-server
- * 
- * 5. npm script_name
+ * 5. npm install --global http-server
  */
