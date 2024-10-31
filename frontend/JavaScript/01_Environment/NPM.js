902b456c92ecb50c608c83526e64c28ad268244a
Cristo RPS <cristobits0101@gmail.com>
2024-10-31 21:15:37 +0000

    Refactoring lines spaces

--- frontend/JavaScript/01_Environment/NPM.js
+++ frontend/JavaScript/01_Environment/NPM.js
@@ -1,9 +1,7 @@
 /**
  * 1.                         npm init -> Inicializar Node.js + parámetros
  * 2.                      npm init -y -> Inicializar Node.js - parámetros
- * 
  * 3.                   node file_name -> Ejecutar archivo JavaScript
  * 4.                  npm script_name -> Ejecutar  script JavaScript
- * 
  * 5. npm install --global http-server -> Instalar servidor web de Node.js
  */
