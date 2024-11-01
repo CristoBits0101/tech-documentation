18f2a527f47048dd674f5c2a16c6294f4944c04b
Cristo RPS <cristobits0101@gmail.com>
2024-11-01 19:23:45 +0000

    Updating examples

--- frontend/JavaScript/04_Functions/Methods.js
+++ frontend/JavaScript/04_Functions/Methods.js
@@ -1,7 +1,20 @@
-// Apply
+const person = { name: 'Ana' }
 
+function greet(greeting) {
+  console.log(`${greeting}, ${this.name}`)
+}
+
+/**
+ * This receives person
+ * Greeting receives 'hi', 'Hey', 'Hello'
+ */
+
+// Apply
+greet.apply(person, ['Hi'])
 
 // Bind
-
+const greetAna = greet.bind(person, 'Hey')
+greetAna()
 
 // Call
+greet.call(person, 'Hello')
