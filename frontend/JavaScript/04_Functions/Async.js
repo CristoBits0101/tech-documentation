41208e406b1f56edd4daa6a3597a428ddfb30640
Cristo RPS <cristobits0101@gmail.com>
2024-11-01 20:20:07 +0000

    Refactoring

--- frontend/JavaScript/04_Functions/Async.js
+++ frontend/JavaScript/04_Functions/Async.js
@@ -4,9 +4,9 @@ async function fetchData() {
     const response = await fetch('https://api.example.com/data')
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
     const data = await response.json()
-    console.log('Datos obtenidos:', data)
+    console.log('Data: ', data)
   } catch (error) {
-    console.error('Error al obtener datos:', error.message)
+    console.error('Error: ', error.message)
   }
 }
 
@@ -15,18 +15,18 @@ fetchData()
 // Callback
 function getData(callback) {
   setTimeout(() => {
-    const data = { id: 1, name: 'Producto' }
-    console.log('Datos obtenidos')
+    const data = { id: 1, name: 'Product' }
+    console.log('Data')
     callback(null, data)
   }, 2000)
 }
 
 getData((error, data) => {
   if (error) {
-    console.error('Error:', error)
+    console.error('Error: ', error)
     return
   }
-  console.log('Callback con datos:', data)
+  console.log('Callback with data: ', data)
 })
 
 // Fetch
@@ -36,10 +36,10 @@ fetch('https://api.example.com/data')
     return response.json()
   })
   .then((data) => {
-    console.log('Datos obtenidos:', data)
+    console.log('Data: ', data)
   })
   .catch((error) => {
-    console.error('Error en la solicitud:', error.message)
+    console.error('Error: ', error.message)
   })
 
 // Promises
