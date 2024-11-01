63ba3a33632fa1846df6b53095d8dd1e2ea9df32
Cristo RPS <cristobits0101@gmail.com>
2024-11-01 19:26:00 +0000

    Refactoring example

--- frontend/JavaScript/04_Functions/Methods.js
+++ frontend/JavaScript/04_Functions/Methods.js
@@ -1,13 +1,9 @@
 const person = { name: 'Ana' }
 
 function greet(greeting) {
-  console.log(`${greeting}, ${this.name}`)
+// console.log(`${'hi', 'Hey', 'Hello'}, ${person.name}`)
+   console.log(`${greeting            }, ${this.name  }`)
 }
-
-/**
- * This receives person
- * Greeting receives 'hi', 'Hey', 'Hello'
- */
 
 // Apply
 greet.apply(person, ['Hi'])
