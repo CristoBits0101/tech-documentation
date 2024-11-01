03829e69baffdb1b68573e4e2d90d4fc4ad1d377
Cristo RPS <cristobits0101@gmail.com>
2024-11-01 11:51:08 +0000

    Refactoring examples

---
+++ frontend/JavaScript/02_Data/Object.js
@@ -0,0 +1,30 @@
+// Call
+const object_name = {
+  ojos: 'blues',
+  pelo: `short`,
+  edad: 17,
+}
+
+console.log(object_name)
+console.log(object_name.edad)
+console.log(object_name['edad'])
+
+// Copy
+const object_name2A = object_name1
+const object_name2B = { ...object_name1 }
+
+// Create
+
+// Examples
+
+// Object.Entries()ForEach()
+
+// Object.Entries()ForOf
+
+// Object.Get
+
+// Object.Keys
+
+// Object.Values
+
+// Sort
