7d23f0fd31fab6902e9c56ce9411ebade55dfd20
Cristo RPS <cristobits0101@gmail.com>
2024-12-24 22:04:10 +0000

    Add TypeScript file with conditional rendering documentation

---
+++ frontend/TypeScript/32_Conditional.ts
@@ -0,0 +1,17 @@
+/**
+ * ??
+ *
+ * If the first value is null or undefined the second is rendered
+ */
+
+/**
+ * &&
+ *
+ * If the first value is true the second is rendered
+ */
+
+/**
+ * ||
+ *
+ * Renders the first or second that returns true
+ */
