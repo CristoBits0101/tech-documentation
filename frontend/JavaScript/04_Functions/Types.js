2411867f8ab59b4b70285dfa8433f3059426eeef
Cristo RPS <cristobits0101@gmail.com>
2024-12-27 10:57:35 +0000

    Simplify arrow function syntax in Types.js

--- frontend/JavaScript/04_Functions/Types.js
+++ frontend/JavaScript/04_Functions/Types.js
@@ -5,9 +5,7 @@ const funcionFlecha6 = () => {
 
 // Arrow
 const funcionFlecha0 = (a) => console.log(a)
-const funcionFlecha2 = (a) => {
-  console.log(a)
-}
+const funcionFlecha2 = (a) => {console.log(a)}
 const funcionFlecha3 = (a, b) => console.log(a, b)
 const funcionFlecha4 = (a, b) => {
   console.log(a, b)
