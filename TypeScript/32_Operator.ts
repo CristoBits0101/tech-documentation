fa5e92e66f5259d14e06efa48734c3c3e9a073da
Cristo RPS <cristobits0101@gmail.com>
2025-01-05 10:05:31 +0000

    Refactor conditional operator documentation: remove obsolete file and add detailed comments in new operator file

--- TypeScript/32_Operator.ts
+++ TypeScript/32_Operator.ts
@@ -1,17 +1,17 @@
 /**
- * ??
+ * Operador ?? (Nullish Coalescing)
  *
  * If the first value is null or undefined the second is rendered
  */
 
 /**
- * &&
+ * Operador && (Logical AND Short-Circuit)
  *
  * If the first value is true the second is rendered
  */
 
 /**
- * ||
+ * Operador || (Logical OR Short-Circuit)
  *
  * Renders the first or second that returns true
  */
