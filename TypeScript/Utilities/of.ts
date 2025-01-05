437c63d2e2902da34203403d52794bcef63798f0
Cristo RPS <cristobits0101@gmail.com>
2025-01-05 13:03:43 +0000

    Refactor TypeScript files: remove obsolete mutability, interception, utility, and guard examples; reorganize relevant code into new locations for better clarity and structure

--- TypeScript/Utilities/of.ts
+++ TypeScript/Utilities/of.ts
@@ -2,3 +2,13 @@
  * typeof     -> Para tipos (boolean, string, number...)
  * instanceof -> Para instancias (date...)
  */
+
+function createAddress() {
+    return {
+        planet: 'Tierra',
+        city: 'Madrid'
+    }
+}
+
+// Crea un type obteniendo las propiedades del retorno
+type AddressReturn = ReturnType<typeof createAddress>
