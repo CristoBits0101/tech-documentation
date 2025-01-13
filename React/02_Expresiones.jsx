c7a5f70b10e8d0210bf73b0e92eaf60e32130a0b
Cristo RPS <cristobits0101@gmail.com>
2025-01-13 08:25:24 +0000

    Refactor Expresiones component and add Declaración component with error comment

--- React/02_Expresiones.jsx
+++ React/02_Expresiones.jsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 export default function Expresiones() {
-  const name = 'John Doe';
+  const name = 'John Doe'
   // Devuelve un valor
-  return <div>{name}</div>;
-}
-
-export default function Declaración() {
-  // Error: Solo se evalúan expresiones
-  // return <div>{if (condition) {}}</div>
+  return <div>{name}</div>
 }
---
+++ React/03_Declaración.jsx
@@ -0,0 +1,4 @@
+export default function Declaración() {
+  // Error: Solo se evalúan expresiones
+  // return <div>{if (condition) {}}</div>
+}
